74fa8d515ad435d660271dd0843860c06c495767
vaibhavswaje <98075983+vaibhavswaje@users.noreply.github.com>
2023-08-12 11:43:29 +0530

    final car-rental application

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect(process.env.URL , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
